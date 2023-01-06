0c44492068671c80d4814bd90bd119a190dd5a69
Gordey <gordeylog71@gmail.com>
2023-01-06 14:11:17 +0300

    working with HTML elements. Creation. Copy

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
-const list = document.querySelector('#list');
+// Выбор контейнера
+const container = document.querySelector('#elementsContainer');
+ 
+// Выбор заголовка
+const newHeader = document.createElement('h1');
+newHeader.innerText = 'Новый заголовок';
 
-list.addEventListener('click', function (event) {
-    console.log(this);
-    console.log(event.target);
-});
+container.append(newHeader);
+
+// Клонирование элементов 
+
+// const mainHeader = document.querySelector('header');
+// const headerCopy = mainHeader.cloneNode(true);
+// container.append(headerCopy);
+
+// Вставка разметки через строки 
+
+// const htmlExample = '<h2>Ещё один заголовок</h2>';
+// container.insertAdjacentHTML('beforeend', htmlExample);
+
+
+// Вставка разметки через шаблонные строки 
+
+const title = 'Текст заголовка';
+const htmlExample = `<h2>${title}</h2>`;
+container.insertAdjacentHTML('beforeend', htmlExample);
