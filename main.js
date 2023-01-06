1a4c7606dd63f70ede5f0283b48a58c68d49806f
Gordey <gordeylog71@gmail.com>
2023-01-06 12:57:20 +0300

    add listening to events. Entering text

--- main.js
+++ main.js
@@ -1,19 +1,8 @@
-const button = document.querySelector('#button'),
-      logo = document.querySelector('#logo');
-
-button.setAttribute('value', 'Удалить изображение');
-
-button.addEventListener('click', function () {
-    console.log('Клик на удаление!');
-    logo.remove();
-
-    button.setAttribute('value', 'Востановить узображение');
-});
+const inputText = document.querySelector('#input-text'),
+      textBlock = document.querySelector('#text-block');
 
 
-
-
-// button.onclick = function () {
-//     console.log('Клик!');
-//     logo.remove();
-// }
+inputText.addEventListener('input', () => {
+    console.log(inputText.value);
+    textBlock.innerText = inputText.value;
+})
