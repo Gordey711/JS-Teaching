ff627883b97fd2b60459a90d27e1eb23bfa53117
Gordey <gordeylog71@gmail.com>
2023-01-06 13:00:48 +0300

    add to way. Listening to events. Entering text

--- main.js
+++ main.js
@@ -2,7 +2,14 @@ const inputText = document.querySelector('#input-text'),
       textBlock = document.querySelector('#text-block');
 
 
-inputText.addEventListener('input', () => {
+// inputText.addEventListener('input', () => {
+//     console.log(inputText.value);
+//     textBlock.innerText = inputText.value;
+// })
+
+inputText.addEventListener('input', inputHandler);
+
+function inputHandler () {
     console.log(inputText.value);
     textBlock.innerText = inputText.value;
-})
+}
